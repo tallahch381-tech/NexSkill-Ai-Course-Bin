e112e4f41224ddaafd41d2d091481a07b079170e
tallahch381-tech <tallahch381@gmail.com>
2026-06-22 06:44:09 -0700

    my code

---
+++ practice22 june.py
@@ -0,0 +1,41 @@
+print("welcome to practise class of 22nd june 2026")
+
+property_id = 784
+
+print(property_id)
+
+print(type(property_id))
+
+property_price = 50000.5
+
+print(property_price)
+
+print(type(property_price))
+
+property_area = "gulberg"
+
+print (property_area)
+
+print(type(property_area))
+
+property_price_5 = property_price * 5
+
+print(property_price_5)
+
+print(type(property_price_5))
+
+property_city = input("Enter the city : ")
+
+print( property_city)
+
+print  (type(property_city))
+
+property_info = input("Enter proprty info:")
+
+print(len(property_info))
+
+for i in property_info:
+
+    print(i)
+
+print(property_info[1:4:1])
