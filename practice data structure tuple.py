1ac6d04f9430126e96a07834cfbca84361939dd9
tallahch381-tech <tallahch381@gmail.com>
2026-06-24 06:54:28 -0700

    my code

---
+++ practice data structure tuple.py
@@ -0,0 +1,12 @@
+bookTuple=(445,"Think and grow rich","napolean hill",50.5)
+
+print(bookTuple)
+print(type(bookTuple))
+print(len(bookTuple))
+for i in bookTuple:
+    print(i)
+
+    print(bookTuple[2])
+    print(type(bookTuple[2]))
+    print(type(bookTuple[3])
+          )
