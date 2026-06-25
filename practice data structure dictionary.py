95075faa31a0a821fdce52ce787b5499109d7ccf
tallahch381-tech <tallahch381@gmail.com>
2026-06-25 01:45:00 -0700

    my code

---
+++ practice data structure dictionary.py
@@ -0,0 +1,17 @@
+bookDictionary= {
+
+    'bookId' :445,
+    'bookTitle': "think and Grow Rich"
+    'bookAuthor' : "Napoleon Hill", 
+    'bookPrice':50.5
+
+}
+print(bookDictionary)
+
+print(type(bookDictionary))
+
+print(len(bookDictionary))
+
+for key in bookDictionary:
+    
+    print(bookDictionary[key])
