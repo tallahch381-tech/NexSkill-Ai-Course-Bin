46c618bbf10f98fb407c0d2002702561475d0244
tallahch381-tech <tallahch381@gmail.com>
2026-06-25 03:22:07 -0700

    MY

--- practice data structureset.py
+++ practice data structureset.py
@@ -1,11 +1,19 @@
 bookSet={445,"Think and grow rich","Napoleon Hill",50.5 }
 
 print(bookSet)
+
+
 print(type(bookSet))
+
 for i in bookSet:
+
     print(i)
+
 bookSet.add(1937)
+
 print(bookSet)
+
 bookSet.discard(1937)
+
 print(bookSet)
  
