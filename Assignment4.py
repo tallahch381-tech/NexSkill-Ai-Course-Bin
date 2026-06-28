2a6e1c451bfe61dfd81d17e69896259fceb9cef5
tallahch381-tech <tallahch381@gmail.com>
2026-06-28 01:34:28 -0700

    my code
    code

---
+++ Assignment4.py
@@ -0,0 +1,76 @@
+print("Hellow ,World!")
+print("TALHA SHAHID")
+
+color=input("black")
+print("black",color)
+
+print("python","programming","language",sep="-")
+
+birth_year=int(input("2010:"))
+age=2026-2010
+print("16",age)
+
+print("The sum of 5 and 5 is ",5+5)
+
+print("Hellow",end=" ")
+print("world")
+
+first=input("programming:")
+second=input("language:")
+print(first+second)
+
+name=("talha:")
+print(("welcome,"+name+"!").upper())
+
+city=("bhawapur:")
+country=input("pakistn:")
+print(city+","+country)
+
+age=20
+print("age is "+str(age))
+
+age=20
+height=5.8
+
+print(type(age))
+print(type(height))
+
+num=3+4j
+print(num)
+print(type(num))
+
+is_python_fun=True
+print(is_python_fun)
+print(type(is_python_fun))
+
+a,b,c=10,20,30,
+print(a)
+print(b)
+print(c)
+
+x= y=z=100
+print(x)
+print(y)
+print(z)
+
+number=float("input(45:")
+print(number)
+print(type(45))
+
+num="100"
+num=int(num)
+print(num)
+print(type(num))
+
+num=7+5j
+print(num.real)
+
+paragraph="python is a powerful programming language used for devolpment, AI ,and data science."
+print(len(paragraph))
+
+a=10
+b=20
+a,b=b,a
+print("a=",a)
+print("b=",b)
+
