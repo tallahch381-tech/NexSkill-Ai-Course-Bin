e1c4ed7f155ada81952644b09a26e67c33a34357
tallahch381-tech <tallahch381@gmail.com>
2026-06-29 21:40:41 -0700

    4
    4

---
+++ practice 29june.py
@@ -0,0 +1,25 @@
+Mobileinfo=input("please enter your mobile no")
+
+print (Mobileinfo)
+
+first5=Mobileinfo[:5:1]
+
+print(first5)
+
+first5=Mobileinfo[0:5:1]
+
+print(first5)
+
+first58=Mobileinfo[0:5:1]
+
+print(first58)
+
+first10to115=Mobileinfo[10:15:1]
+
+print(first10to115)
+
+firsttillend=Mobileinfo[0:5:1]
+
+print(firsttillend)
+
+
---
+++ practice week 1.py
@@ -0,0 +1,11 @@
+mobileName=input("oppo a53")
+print("oppo a53")
+print(type(mobileName))
+print(len(mobileName))
+for x in mobileName:
+    print(x)
+    # MY first code ends here
+
+    mobileprice=input("oppo a53")
+    print(type(mobileprice))
+    print(len(mobileprice))
