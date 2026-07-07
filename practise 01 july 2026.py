0c3bf527cf4be2790ade4da4b633e35337d8f8d7
tallahch381-tech <tallahch381@gmail.com>
2026-07-06 22:09:02 -0700

    Update practise 01 july 2026.py

--- practise 01 july 2026.py
+++ practise 01 july 2026.py
@@ -1,1 +1,8 @@
 import numpy as np
+ids, price , long , lat = np.genfromtxt('Week4/zameencom-property-data-By-Kaggle-short.csv', delimiter=';', usecols=(0,4,8,9), unpack=True, dtype=None,skip_header=1)
+print(ids)
+print(price)
+print(long)
+print(lat)
+
+print()
