cb85042f2c6909458e5652eef4350b8a4a78bbb4
tallahch381-tech <tallahch381@gmail.com>
2026-07-14 05:42:54 -0700

    Update practise 01 july 2026.py

--- practise 01 july 2026.py
+++ practise 01 july 2026.py
@@ -72,3 +72,230 @@ print("Zameen.com Price - div - pie   - Hyperbolic Cosine values:", cosh_values)
 # Calculate the hyperbolic tangent of each element
 tanh_values = np.tanh(pricePie)
 print("Zameen.com Price - div - pie   -Hyperbolic Tangent values:", tanh_values)
+
+#Example: Inverse Hyperbolic Sine
+
+# Calculate the inverse hyperbolic sine of each element
+asinh_values = np.arcsinh(pricePie)
+print("Zameen.com Price - div - pie   -Inverse Hyperbolic Sine values:", asinh_values)
+
+#Example: Inverse Hyperbolic Cosine
+# Calculate the inverse hyperbolic cosine of each element
+acosh_values = np.arccosh(pricePie)
+print("Zameen.com Price - div - pie   -Inverse Hyperbolic Cosine values:", acosh_values)
+
+
+#Zameen.com Long Plus Lat - 2 dimentional arrary
+D2LongLat = np.array([long,
+                  lat])
+
+print ("Zameen.com Long Plus Lat - 2 dimentional arrary - " ,D2LongLat)
+
+# check the dimension of array1
+print("Zameen.com Long Plus Lat - 2 dimentional arrary - dimension" , D2LongLat.ndim) 
+# Output: 2
+
+# return total number of elements in array1
+print("Zameen.com Long Plus Lat - 2 dimentional arrary - total number of elements" ,D2LongLat.size)
+# Output: 6
+
+# return a tuple that gives size of array in each dimension
+print("Zameen.com Long Plus Lat - 2 dimentional arrary - gives size of array in each dimension" ,D2LongLat.shape)
+# Output: (2,3)
+
+# check the data type of array1
+print("Zameen.com Long Plus Lat - 2 dimentional arrary - data type" ,D2LongLat.dtype) 
+# Output: int64
+
+# Splicing array
+D2LongLatSlice=  D2LongLat[0:1:1 , 1:5:1]
+print("Zameen.com Long Plus Lat - 2 dimentional arrary - Splicing array - D2LongLat[:1,:5] " , D2LongLatSlice)
+D2LongLatSlice2=  D2LongLat[:1, 4:15:4]
+print("Zameen.com Long Plus Lat - 2 dimentional arrary - Splicing array - D2LongLat[:1, 4:15:4] " , D2LongLatSlice2)
+
+
+
+# Indexing array
+D2LongLatSliceItemOnly=  D2LongLatSlice[0,1]
+print("Zameen.com Long Plus Lat - 2 dimentional arrary - Index array - D2LongLatSlice[1,5] " , D2LongLatSliceItemOnly)
+D2LongLatSlice2ItemOnly=  D2LongLatSlice2[0, 2]
+print("Zameen.com Long Plus Lat - 2 dimentional arrary - index array - D2LongLatSlice2[0, 2] " , D2LongLatSlice2ItemOnly)
+
+
+#You should use the builtin function nditer, if you don't need to have the indexes values.
+for elem in np.nditer(D2LongLat):
+    print(elem)
+
+#EDIT: If you need indexes (as a tuple for 2D table), then:
+for index, elem in np.ndenumerate(D2LongLat):
+    print(index, elem)
+
+"""# for loop
+rows = np.shape(D2LongLat[0])[0]
+cols = np.shape(D2LongLat[1])[0]
+for i in range(0, (rows + 1)):
+    for j in range(0, (cols + 1)):
+        print (D2LongLat[i,j])
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
